acde119c2b8dbc6456efb87b08aab21db46dda04
jinghann <32559567+jinghann@users.noreply.github.com>
2021-04-06 16:10:07 +0800

    Update settings.py

--- vsgenerator/settings.py
+++ vsgenerator/settings.py
@@ -148,6 +148,7 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ntufyp20demo@gmail.com'
-EMAIL_HOST_PASSWORD = '3ydkycz5'
 EMAIL_USE_TLS = True
+# Set your host email account here
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
